ccfefcce38c98719f76b695baad006eafd73a200
davisdeatonphotography <davis.deaton@verizon.net>
2025-10-30 15:47:16 -0500

    refactor: Simplify deployment process by removing git pull and updating workflow steps
    - Removed the git pull command from deploy.sh to streamline the deployment process.
    - Updated the GitHub Actions workflow to use actions/checkout@v4 for checking out the repository, replacing the previous force reset method.
    - These changes enhance the deployment efficiency and maintainability of the workflow.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,6 @@
 echo ">>> Navigating to the application directory..."
 cd /home/davis/app
 
-echo ">>> Pulling latest changes from the 'main' branch..."
-git pull origin main
-
 # Create the permanent database directory if it doesn't exist
 echo ">>> Ensuring database directory exists..."
 mkdir -p /home/davis/database/mongo
