f6a5d81e8d3c3b3438f26b9c7f4b7c4c1b055f54
davisdeatonphotography <davis.deaton@verizon.net>
2026-04-17 14:55:18 -0600

    feat: remove pick layer from trip map renderer to streamline rendering process

--- static/js/modules/trip-map-renderer.js
+++ static/js/modules/trip-map-renderer.js
@@ -7,7 +7,6 @@ import tripInteractions from "./trip-interactions.js";
 
 const TRIP_LAYER_NAMES = new Set(["trips", "matchedTrips"]);
 const WORKER_URL = new URL("./trip-map-worker.js", import.meta.url);
-const PICK_WIDTH_PX = 14;
 
 function isTripLayer(layerName) {
   return TRIP_LAYER_NAMES.has(layerName);
@@ -267,7 +266,6 @@ const tripMapRenderer = {
           opacity: layerInfo.opacity ?? 1,
           onClick: (info) => this.handleTripClick(info, layerName),
         }),
-        this.buildPickLayer(layerName, common),
       ];
     }
 
@@ -281,22 +279,7 @@ const tripMapRenderer = {
         opacity: layerInfo.opacity ?? 1,
         onClick: (info) => this.handleTripClick(info, layerName),
       }),
-      this.buildPickLayer(layerName, common),
     ];
-  },
-
-  buildPickLayer(layerName, common) {
-    return new deck.PathLayer({
-      ...common,
-      id: `${layerName}-trip-map-pick`,
-      pickable: true,
-      getColor: [0, 0, 0, 1],
-      getWidth: PICK_WIDTH_PX,
-      widthMinPixels: PICK_WIDTH_PX,
-      widthMaxPixels: PICK_WIDTH_PX,
-      opacity: 0.01,
-      onClick: (info) => this.handleTripClick(info, layerName),
-    });
   },
 
   buildSelectedLayers() {
